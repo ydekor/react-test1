e32a94708db6e2583bb39da3241d2b3b2344f011
ydekor <114324139+ydekor@users.noreply.github.com>
2023-05-22 20:58:13 +0300

    edit App.js and App.js

--- src/app/App.js
+++ src/app/App.js
@@ -3,10 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <div className="testWindow">
+          aboba
+          123
+      </div>
     </div>
   );
 }
-
 export default App;
