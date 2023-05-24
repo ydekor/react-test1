5fb8d91406a43ace7148b6301f8500b779c654d6
ydekor <114324139+ydekor@users.noreply.github.com>
2023-05-24 15:43:56 +0300

    a side menu is displayed depending on the setting value

--- src/app/leftPanel/LeftPanel.js
+++ src/app/leftPanel/LeftPanel.js
@@ -3,7 +3,7 @@ import style from './LeftPanel.module.css';
 function LeftPanel({isVisible}) {
     return (
         // <div className={`${style.sidePanel} ${isVisible ? '' : style.hidden}`}>111</div>
-        <div><div>{isVisible && <div className={style.sidePanel}>111</div>}</div></div>
+        <div>{isVisible && <div className={style.sidePanel}>111</div>}</div>
     );
 }
 export default LeftPanel;
