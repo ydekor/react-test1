2ae508441319f557a4553efe27abee2b2a74af27
ydekor <114324139+ydekor@users.noreply.github.com>
2023-05-28 18:39:04 +0300

    variable names changed

--- src/app/App.js
+++ src/app/App.js
@@ -4,14 +4,12 @@ import TopLine from "./top/Top";
 import LeftPanel from "./leftPanel/LeftPanel";
 import Content from "./content/Content";
 
-
 function App() {
     const [sidePanelVisible, setSidePanelVisible] = useState(false);
 
     const toggleSidePanel = () => {
         setSidePanelVisible(!sidePanelVisible);
     }
-
 
     return <div className={style.App}>
         <TopLine toggleSidePanel={toggleSidePanel}/>
--- src/app/content/Content.js
+++ src/app/content/Content.js
@@ -5,103 +5,64 @@ import React, {useState} from "react";
 //const comments = { id: 1, name: 'user1', created: 'da', comment: 'text'}
 //const comments = [{id: 1}, {name: 'user1'}, {created: 'da'}, {comment: 'text'}];
 
-const arr = [
+const CommentsArr = [
     {
         id: 1,
         name: 'anon1',
-        created: '14.08.28',
+        created: new Date().toLocaleDateString(),
         comment: 'awesome'
     },
     {
         id: 2,
         name: 'anon2',
-        created: '11.09.15',
+        created: new Date().toLocaleDateString(),
         comment: 'hi'
     },
     {
         id: 3,
         name: 'anon3',
-        created: '22.02.13',
+        created: new Date().toLocaleDateString(),
         comment: 'kal'
     },
     {
         id: 4,
         name: 'anon4',
-        created: '22.02.13',
+        created: new Date().toLocaleDateString(),
         comment: 'va'
-    },
-    {
-        id: 5,
-        name: 'anon5',
-        created: '22.02.13',
-        comment: 'suck'
-    },
-
-    {
-        id: 6,
-        name: 'anon6',
-        created: '22.02.13',
-        comment: 'ra'
-    },
-    {
-        id: 7,
-        name: 'anon7',
-        created: '22.02.13',
-        comment: 'huh'
-    },
-    {
-        id: 8,
-        name: 'anon8',
-        created: '22.02.13',
-        comment: 'uwu'
-    },
-    {
-        id: 9,
-        name: 'anon9',
-        created: '22.02.13',
-        comment: 'lmao'
-    },
-    {
-        id: 10,
-        name: 'anon10',
-        created: '22.02.13',
-        comment: 'G-man'
     },
 ]
 
 function Content() {
-    const [text1, setText1] = useState('');
-    const [text2, setText2] = useState('');
-    const [comments, setComments] = useState(arr);
+    const [nameText, setName] = useState('');
+    const [commentText, setCommentText] = useState('');
+    const [comments, setComments] = useState(CommentsArr);
 
-    const handleChange1 = (event) => {
-        setText1(event.target.value);
+    const nameOnChange = (event) => {
+        setName(event.target.value);
     }
-    const handleChange2 = (event) => {
-        setText2(event.target.value);
+    const commentOnChange = (event) => {
+        setCommentText(event.target.value);
     }
 
-    const sendData = () => {
-        const newArr = {
+    const addComment = () => {
+            const newCommentsArr = {
             id: comments.length + 1,
-            name: text1,
+            name: nameText,
             created: new Date().toLocaleDateString(),
-            comment: text2,
+            comment: commentText,
         }
-        setComments([...comments, newArr]);
-        setText1('');
-        setText2('');
+        setComments([...comments, newCommentsArr]);
+        setName('');
+        setCommentText('');
     }
 
-    return <div className={style.testWindow}>
+    return <div className={style.mainWindow}>
         <div className={style.inputData}>
-            <div><input type="text" className={style.inputName} placeholder={"input name"} value={text1} onChange={handleChange1} /></div>
-            <div><input type="text" className={style.inputComment} placeholder={"input comment"} value={text2} onChange={handleChange2} /></div>
-            {/*<p>{text1}</p>*/}
-            {/*<p>{text2}</p>*/}
+            <div><input type="text" className={style.inputName} placeholder={"input name"} value={nameText} onChange={nameOnChange} /></div>
+            <div><input type="text" className={style.inputComment} placeholder={"input comment"} value={commentText} onChange={commentOnChange} /></div>
         </div>
         <div className={style.sendData}>
-            <button onClick={sendData}>send</button>
+            <button onClick={addComment}>send</button>
         </div>
         <div>
             {comments.map(e => <Comment
