735d5324e7200bc2756c94e2604184d60f5ccf3d
ydekor <114324139+ydekor@users.noreply.github.com>
2023-06-01 15:56:57 +0300

    changed the method of transferring the name and login

--- src/app/leftPanel/LeftPanel.js
+++ src/app/leftPanel/LeftPanel.js
@@ -1,18 +1,23 @@
 import style from './LeftPanel.module.css';
 
 function LeftPanel({setCurrentView}) {
+    const View = {
+        Main: 'main',
+        Login: 'login'
+    }
+
     const showMain = () => {
-        setCurrentView('main');
+        setCurrentView(View.Main);
     }
     const showLogin = () => {
-        setCurrentView('login');
+        setCurrentView(View.Login);
     }
 
     return (
         <div className={style.sidePanel}>
             <div className={style.buttons}>
-                <div><button className={style.button} onClick={showMain}>main</button></div>
-                <div><button className={style.button} onClick={showLogin}>login</button></div>
+                <div><button className={style.button} onClick={showMain}>{View.Main}</button></div>
+                <div><button className={style.button} onClick={showLogin}>{View.Login}</button></div>
             </div>
         </div>
     );
